e6ab2420047f36d8845e3092ac3bbfa592224d3c
Caroline Ininga <carolineininga@gmail.com>
2025-03-30 17:03:49 +0300

    completed lab on dom modifying lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+
+
+let main = document.querySelector('#main');
+main.remove();
+
+
+// let newh1 =
+// let body = document.querySelector('body');
+// body.append(newh1);
+
+
+
+let newHeader =  document.createElement('h1');
+newHeader.id = "victory";
+
+newHeader.textContent = 'CAROLINE ININGA is the champion'
